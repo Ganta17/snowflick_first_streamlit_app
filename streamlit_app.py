7cd4c917dca7b9498cf5c43232d3570b288b4fca
Ganta17 <78079019+Ganta17@users.noreply.github.com>
2023-07-05 16:25:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,18 +47,30 @@ except URLError as e:
 streamlit.stop()
 #importing 
 import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-
-my_cur.execute("USE ROLE ACCOUNTADMIN")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_row = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
-#streamlit.dataframe(my_data_row)
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("USE ROLE ACCOUNTADMIN")
+          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+
+          my_data_row = my_cur.fetchall()
+          return my_data_row
+          
+if streamlit.button('Get fruit load list'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_row = get_fruit_load_list()
+     streamlit.dataframe(my_data_row)
+     #my_cur = my_cnx.cursor()
+     #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+# my_cur.execute("USE ROLE ACCOUNTADMIN")
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+#my_data_row = my_cur.fetchone()
+# my_data_row = my_cur.fetchall()
+# #streamlit.text("Hello from Snowflake:")
+# streamlit.text("The fruit load list contains:")
+# streamlit.text(my_data_row)
+# #streamlit.dataframe(my_data_row)
 
 fruit_choice_1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice_1)
