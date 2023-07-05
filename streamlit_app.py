b9bac122722eae022ccc08cde5f0adfdf46e4998
Ganta17 <78079019+Ganta17@users.noreply.github.com>
2023-07-05 16:20:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_fruitvice_data(this_fruit_choice):
      return fruityvice_normalized
     
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
     else:
